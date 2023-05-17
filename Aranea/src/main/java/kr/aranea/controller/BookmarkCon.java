9a0d4878661e19fa8262b2419036d23c688a8ea5
smhrd <smhrd@211.105.165.112>
2023-05-17 18:27:45 +0900

    css 틀 수정

---
+++ Aranea/src/main/java/kr/aranea/controller/BookmarkCon.java
@@ -0,0 +1,20 @@
+package kr.aranea.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BookmarkCon implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String nextview = "bookmark";
+		
+		return nextview;
+	}
+
+}
