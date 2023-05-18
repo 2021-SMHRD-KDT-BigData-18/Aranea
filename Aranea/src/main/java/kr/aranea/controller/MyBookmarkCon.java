6aaef6632cb5e842429663540257ef70913cd726
smhrd <smhrd@121.147.185.89>
2023-05-18 14:17:46 +0900

    이경민 - 찜

--- Aranea/src/main/java/kr/aranea/controller/MyBookmarkCon.java
+++ Aranea/src/main/java/kr/aranea/controller/MyBookmarkCon.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import kr.aranea.dao.T_CommodityDAO;
 import kr.aranea.dao.T_LikeDAO;
 import kr.aranea.entity.T_Commodity;
 import kr.aranea.entity.T_Like;
+import kr.aranea.entity.T_User;
 
 public class MyBookmarkCon implements Controller {
 
@@ -19,6 +21,10 @@ public class MyBookmarkCon implements Controller {
 
 		request.setCharacterEncoding("UTF-8");
 		
+		HttpSession session = request.getSession();
+
+		T_User user = (T_User)session.getAttribute("user");
+		
 		int cm_seq = Integer.parseInt(request.getParameter("cm_seq"));
 		
 		T_CommodityDAO dao = new T_CommodityDAO();
@@ -27,18 +33,21 @@ public class MyBookmarkCon implements Controller {
 		String cm_category = view.getCm_category();
 		String cm_name = view.getCm_name();
 		String cm_price = view.getCm_price();
-		
+		String user_id = user.getUser_id();
 		
 		System.out.println(cm_seq);
 		System.out.println(cm_category);
 		System.out.println(cm_name);
 		System.out.println(cm_price);
+		System.out.println(user_id);
+		
 		
 		T_Like dto = new T_Like();
 		dto.setCm_seq(cm_seq);
 		dto.setCm_name(cm_name);
 		dto.setCm_category(cm_category);
 		dto.setCm_price(cm_price);
+		dto.setUser_id(user_id);
 		
 		T_LikeDAO daoo = new T_LikeDAO();
 		int row = daoo.insert(dto);
--- Aranea/src/main/java/kr/aranea/entity/T_Like.java
+++ Aranea/src/main/java/kr/aranea/entity/T_Like.java
@@ -9,15 +9,18 @@ public class T_Like {
 	private String cm_category;
 	
 	private String cm_price;
+	
+	private String user_id; 
 
 	public T_Like() {}
-	
-	public T_Like(int cm_seq, String cm_name, String cm_category, String cm_price) {
+
+	public T_Like(int cm_seq, String cm_name, String cm_category, String cm_price, String user_id) {
 		super();
 		this.cm_seq = cm_seq;
 		this.cm_name = cm_name;
 		this.cm_category = cm_category;
 		this.cm_price = cm_price;
+		this.user_id = user_id;
 	}
 
 	public int getCm_seq() {
@@ -51,8 +54,18 @@ public class T_Like {
 	public void setCm_price(String cm_price) {
 		this.cm_price = cm_price;
 	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
 	
 	
 	
 	
+	
+		
 }
