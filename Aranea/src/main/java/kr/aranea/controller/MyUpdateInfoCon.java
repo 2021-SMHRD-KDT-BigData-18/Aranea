601d7fad1d2f7eb7ffb6efad4fc66e4e4a3a6dee
Egeg <Egeg@119.206.166.57>
2023-05-18 16:41:17 +0900

    현주 - 정보수정 업데이트

--- Aranea/src/main/java/kr/aranea/controller/MyUpdateInfoCon.java
+++ Aranea/src/main/java/kr/aranea/controller/MyUpdateInfoCon.java
@@ -5,12 +5,27 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kr.aranea.dao.T_UserDAO;
+import kr.aranea.entity.T_User;
 
 public class MyUpdateInfoCon implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		T_User user = (T_User)session.getAttribute("user");
+		
+		String user_id = user.getUser_id();
+		
+		T_UserDAO dao = new T_UserDAO();
+		T_User list = dao.selectUi(user_id);
+
+		
+		session.setAttribute("list2", list);
 
 		String nextview = "myupdateInfo";
 		
